975d5d2457881ad826b4a5f396c09fd35b0336f9
RoniPG <ronigmn@gmail.com>
2025-02-02 20:42:42 +0100

    BONUS - Iteration 9: Refactoring with Subclasses and Inheritance

--- js/obstacle.js
+++ js/obstacle.js
@@ -1,26 +1,17 @@
-class Obstacle {
+class Obstacle extends Component{
     constructor(gameScren) {
-        this.gameScren = gameScren;
-        this.left = Math.round(Math.random() * 300 + 70);
-        this.top = 0;
-        this.width = 100;
-        this.height = 150;
-        this.element = document.createElement('img');
-        this.element.src = './images/redCar.png';
-        this.element.style.position = 'absolute';
-        this.element.style.width = `${this.width}px`;
-        this.element.style.height = `${this.height}px`;
-        this.element.style.top = `${this.top}px`;
-        this.element.style.left = `${this.left}px`;
-        this.gameScren.appendChild(this.element);
+        super(
+            gameScren, 
+            Math.round(Math.random) * 300 + 70,
+            0,
+            100,
+            150,
+            './images/redCar.png'
+        );
     }
+
     move() {
         this.top += 3;
         this.updatePosition();
-    }
-
-    updatePosition() {
-        this.element.style.top = `${this.top}px`;
-        this.element.style.left = `${this.left}px`;
     }
 }
--- js/player.js
+++ js/player.js
@@ -1,21 +1,8 @@
-class Player {
+class Player  extends Component{
     constructor(gameScreen, left, top, width, height, url) {
-        this.gameScreen = gameScreen;
-        this.left = left;
-        this.top = top;
-        this.width = width;
-        this.height = height;
+        super(gameScreen, left, top, width, height, url);
         this.directionX = 0;
         this.directionY = 0;
-        this.element = document.createElement('img');
-        this.element.src = url;
-        this.element.style.position = 'absolute';
-        this.element.style.width = `${this.width}px`
-        this.element.style.height = `${this.height}px`
-        this.element.style.left = `${this.left}px`
-        this.element.style.top = `${this.top}px`
-
-        this.gameScreen.appendChild(this.element);
     }
     move () {
         this.left += this.directionX;
@@ -30,10 +17,7 @@ class Player {
         }
         this.updatePosition();
     }
-    updatePosition() {
-        this.element.style.left = `${this.left}px`;
-        this.element.style.top = `${this.top}px`;
-    }
+
     didCollide(obstacle) {
         const playerRect = this.element.getBoundingClientRect();
         const obstacleRect = obstacle.element.getBoundingClientRect();
